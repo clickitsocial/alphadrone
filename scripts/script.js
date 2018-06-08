13f69a507608b3b53a7e1f89e3d057c49c966b99
Bryan Almaraz <bryan.almaraz96@gmail.com>
2018-06-08 11:41:33 -0700

    Hamburger Nav Fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
+
+var sideNavBar = document.querySelector('.side-nav');
+var ham = document.querySelector('#nav-icon3');
+// Hamburger nav transition
 $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
-	});
+    });
+    
+    // add width
+    ham.addEventListener('click',function(){
+        sideNavBar.classList.toggle('toggle-nav');
+         
+    })
 });
