12c131470fa15ca11154a1161d2cd525377f2443
Bryan Almaraz <bryan.almaraz96@gmail.com>
2018-06-12 10:50:02 -0700

    mobile nav done

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,30 @@ var sideNavBar = document.querySelector('.side-nav');
 var navList = document.querySelector('aside ul');
 var navListLi = document.querySelector('aside ul li');
 var ham = document.querySelector('#nav-icon3');
+
+// mobile nav elements
+var mobileNav = document.querySelector('#landing-page-nav');
+var mobileNavList = document.querySelector('#landing-page-nav ul');
+var mobileListLi = document.querySelector('#landing-page-nav ul li');
+var mobileHam = document.querySelector('#mobile-ham-nav')
 // Hamburger nav transition
 $(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+	$('#mobile-ham-nav,#nav-icon3').click(function(){
 		$(this).toggleClass('open');
     });
     
-    // add width
+    // Add Width
+
+    // bigger screens
     ham.addEventListener('click',function(){
         sideNavBar.classList.toggle('toggle-nav');
         navList.classList.toggle('side-nav-links-closed');
-        console.log(sideNavBar);
-        console.log(navList);
     })
+
+    // mobile screen
+    mobileHam.addEventListener('click',function(){
+        mobileNav.classList.toggle('open');
+        mobileNavList.classList.toggle('open');
+    })
+
 });
